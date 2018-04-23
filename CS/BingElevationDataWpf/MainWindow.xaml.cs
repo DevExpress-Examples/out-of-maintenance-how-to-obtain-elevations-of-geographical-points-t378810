9a2582b547d533fac3ff6c5186a56d071978c0b3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BingElevationDataWpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Xpf.Map;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace BingElevationDataWpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            List<GeoPoint> locations = new List<GeoPoint> {
+                new GeoPoint(41.145556, -73.995),
+                new GeoPoint(36.131389, -90.937222),
+                new GeoPoint(32.175, -80.136389)
+            };
+            provider.RequestPointsElevations(locations);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to obtain elevations of geographical points
+
+
+This example demonstrates how to receive elevation data from the Bing Map Service.
+
+
+<h3>Description</h3>
+
+To do this, request the required elevation information using the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapBingElevationDataProvider_RequestPointsElevationstopic(ZsR7ew)">BingElevationDataProvider.RequestPointsElevations</a>&nbsp;method.
+
+<br/>
+
+
